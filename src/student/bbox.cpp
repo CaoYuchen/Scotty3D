6ae22c08b0071d7cdbd5426b06999dbde087d32d
Joshua Rain <1014054551@qq.com>
2021-11-10 23:37:30 -0500

    bbox

--- src/student/bbox.cpp
+++ src/student/bbox.cpp
@@ -8,6 +8,34 @@ bool BBox::hit(const Ray& ray, Vec2& times) const {
     // Implement ray - bounding box intersection test
     // If the ray intersected the bounding box within the range given by
     // [times.x,times.y], update times with the new intersection times.
+    float t_min = (min.x - ray.point.x) / ray.dir.x;
+    float t_max = (max.x - ray.point.x) / ray.dir.x;
 
-    return false;
+    if(t_min > t_max) std::swap(t_min, t_max);
+
+    float ty_min = (min.y - ray.point.y) / ray.dir.y;
+    float ty_max = (max.y - ray.point.y) / ray.dir.y;
+
+    if(ty_min > ty_max) std::swap(ty_min, ty_max);
+
+    if((t_min > ty_max) || (ty_min > t_max)) return false;
+
+    if(ty_min > t_min) t_min = ty_min;
+
+    if(ty_max < t_max) t_max = ty_max;
+
+    float tz_min = (min.z - ray.point.z) / ray.dir.z;
+    float tz_max = (max.z - ray.point.z) / ray.dir.z;
+
+    if(tz_min > tz_max) std::swap(tz_min, tz_max);
+
+    if((t_min > tz_max) || (tz_min > t_max)) return false;
+
+    if(tz_min > t_min) t_min = tz_min;
+
+    if(tz_max < t_max) t_max = tz_max;
+
+    times.x = t_min;
+    times.y = t_max;
+    return true;
 }
--- src/student/shapes.cpp
+++ src/student/shapes.cpp
@@ -24,13 +24,35 @@ Trace Sphere::hit(const Ray& ray) const {
     // ray.dist_bounds! For example, if there are two intersections,
     // but only the _later_ one is within ray.dist_bounds, you should
     // return that one!
-
+    BBox box;
+    Vec3 c = box.center();
+    float r = radius;
+    float b = dot(ray.point - c, ray.dir);
+    float b2_ac = b * b - dot(ray.point - c, ray.point - c) + r * r;
+    float t1, t2, t;
     Trace ret;
+
+    if(b2_ac >= 0) {
+        t1 = -1 * b + sqrt(b2_ac);
+        t2 = -1 * b - sqrt(b2_ac);
+        if(t1 < ray.dist_bounds.x) {
+            if(t2 > ray.dist_bounds.y || t2 < ray.dist_bounds.x)
+                return ret;
+            else
+                t = t2;
+        } else if(t1 > ray.dist_bounds.y) {
+            return ret;
+        } else {
+            t = t1;
+        }
+    } else
+        return ret;
+
     ret.origin = ray.point;
-    ret.hit = false;       // was there an intersection?
-    ret.distance = 0.0f;   // at what distance did the intersection occur?
-    ret.position = Vec3{}; // where was the intersection?
-    ret.normal = Vec3{};   // what was the surface normal at the intersection?
+    ret.hit = true;                      // was there an intersection?
+    ret.distance = t;                    // at what distance did the intersection occur?
+    ret.position = ray.at(t);            // where was the intersection?
+    ret.normal = (ray.at(t) - c).unit(); // what was the surface normal at the intersection?
     return ret;
 }
 
